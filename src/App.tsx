05a5866a35716a9505f2a32b9e2813711f86a08e
Viviane110199 <vivianerego22@gmail.com>
2022-04-29 18:01:45 -0300

    Subindo arquivos

---
+++ src/App.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import Navbar from './components/estaticos/navbar/Navbar';
+import Footer from './components/estaticos/footer/Footer';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Login from './paginas/login/Login';
+import Home from './paginas/home/Home';
+import './App.css';
+
+
+function App() {
+  return (
+    <Router>
+      <Navbar />
+      <div style={{ minHeight: '100vh' }}>
+        <Routes> 
+          <Route path="/" element={<Login />} />
+           <Route path="/login" element={<Login />} />
+           <Route path="/home" element={<Home />} />
+         {/* <Route path="/cadastro" element={<CadastroUsuario />} /> */}
+        </Routes>
+      </div>
+      <Footer />
+    </Router>
+  );
+}
+
+export default App
